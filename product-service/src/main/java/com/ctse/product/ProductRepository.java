f3221206b1ca48c023092df234b730854ceda5cb
Thamindu Vimansha <159893134+Tommy14@users.noreply.github.com>
2026-03-02 17:01:00 +0530

    refactor(product-service): drop legacy ProductRepository from flat package

--- product-service/src/main/java/com/ctse/product/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-﻿package com.ctse.product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByCategory(String category);
-    List<Product> findByStockGreaterThan(int stock);
-}
