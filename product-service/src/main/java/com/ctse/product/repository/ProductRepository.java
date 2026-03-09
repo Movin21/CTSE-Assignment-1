ec4d9b1e53009e50c1c06c4ed3ab60274c3f3b50
Thamindu Vimansha <159893134+Tommy14@users.noreply.github.com>
2026-03-09 16:49:00 +0530

    feat(product-service): add ProductRepository extending JpaRepository

---
+++ product-service/src/main/java/com/ctse/product/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.ctse.product.repository;
+
+import com.ctse.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+}
